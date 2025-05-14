dd16b6e23a968d9606d820c7e8ee807f2bf0249e
JayModded <78915475+JayModded0@users.noreply.github.com>
2025-05-13 23:05:45 -0400

    Update sinclientgrading.py

--- sinclientgrading.py
+++ sinclientgrading.py
@@ -10,10 +10,8 @@ def check_public_ip(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent)
     gradinglib.vm_execute_firewalloff_command(vm, guest_username, guest_password, content)
 
     try:
-        # Use ipconfig for Windows
         output = gradinglib.execute_vm_command(vm, guest_username, guest_password, "ipconfig", content, "cmd.exe")
         
-        # Adjust regex to match Windows IP address format
         regex = "(?<=IPv4 Address[^\d]*)(\d+.\d+.\d+.\d+)"
         match = re.search(regex, output)
         
@@ -23,17 +21,18 @@ def check_public_ip(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent)
         
         ip_address = match.group(1)
         logger.debug(f"Extracted IP address: {ip_address}")
-        
-        # Extract the last octet of the IP address
+
         last_octet = ip_address.split('.')[-1]
         pfsensepublic = "10" + last_octet
         teamname = vm.parent.name
         teamnumber = gradinglib.team_name_to_number(vm.parent.name)
 
-        if pfsensepublic == "10" + str(teamnumber):
-            logger.info(f"SUCCESS! {vm.name} in {vm.parent.name} passed. Public IP is correct.")
-        else:
-            logger.warn(f"FAILED! {vm.name} in {vm.parent.name} failed. Public IP is incorrect.")
+        if pfsensepublic.startswith("10.42.") and pfsensepublic.endswith(".152"):
+            x = int(pfsensepublic.split(".")[2]) 
+            if x == gradinglib.team_name_to_number(vm.parent.name):
+                logger.info(f"SUCCESS! {vm.name} in {vm.parent.name} passed. Public IP is correct.")
+            else:
+                logger.warning(f"FAILED! {vm.name} in {vm.parent.name} failed. Public IP is incorrect.")
         print(output)
     except vim.fault.GuestOperationsUnavailable as e:
         logger.warn(f"FAILED! {vm.name} in {vm.parent.name} failed. Guest operations unavailable.")
@@ -41,6 +40,3 @@ def check_public_ip(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent)
     except Exception as e:
         logger.error(f"An error occurred: {e}")
         return
-    finally:
-        gradinglib.power_off_vm(vm)
-        logger.debug(f"Powering off {vm.name}...")
