bf9755c438c382c45d97c65e4300624d595904a7
Blake <80996688+BlakeTnr@users.noreply.github.com>
2025-05-13 20:20:25 -0400

    Check ADAdmin in secdev

--- windowsgrading.py
+++ windowsgrading.py
@@ -2,7 +2,7 @@ import gradinglib
 from logger import logger
 from pyVmomi import vim
 
-def check_adadmin_user(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent):
+def check_adadmin_in_secdev(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent):
     guest_username, guest_password = gradinglib.get_vm_creds(vm)
 
     try:
@@ -19,7 +19,24 @@ def check_adadmin_user(vm: vim.VirtualMachine, content: vim.ServiceInstanceConte
         logger.warn(f"FAILED! {vm.name} in {vm.parent.name} failed. Guest operations unavailable.")
         return
 
+def check_adadmin_in_secdev(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent):
+    guest_username, guest_password = gradinglib.get_vm_creds(vm)
+
+    try:
+        output = gradinglib.execute_vm_command(vm, guest_username, guest_password, "net user ADAdmin", content, "powershell")
+        regex = "SecDev"
+        match = regex.findall(output)
+
+        if len(match) == 1:
+            logger.info(f"SUCCESS! {vm.name} in {vm.parent.name} passed. ADAdmin is in SecDev.")
+        else:
+            logger.warn(f"FAILED! {vm.name} in {vm.parent.name} failed. ADAdmin is in SecDev.")
+
+    except vim.fault.GuestOperationsUnavailable as e:
+        logger.warn(f"FAILED! {vm.name} in {vm.parent.name} failed. Guest operations unavailable.")
+        return
 
 def check_ad(vm: vim.VirtualMachine, content: vim.ServiceInstanceContent):
     logger.debug(f"Running AD checks for {vm.name}...")
-    check_adadmin_user(vm, content)
+    check_adadmin_in_secdev(vm, content)
+    check_adadmin_in_secdev(vm, content)
